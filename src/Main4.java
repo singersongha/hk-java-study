b08b35c1cfeda22762f4ce4a83be65155a8eb135
김송하 <songha714@naver.com>
2024-09-23 23:08:02 +0900

    3-3-2

--- src/Main4.java
+++ src/Main4.java
@@ -21,28 +21,24 @@ public class Main4 {
         System.out.println("---------------");
 
 //      3
-        for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j <= 2; j++) {
-                if(i == 1 || i == 2) {
-                    System.out.print(arr1[i][j]);
-                }else if(i == 0){
-                    System.out.print(arr1[i][j]);
-                }
-
-            }
+        for (int i = 1; i <= 2; i++) {
+            System.out.print(arr1[i][1]);
+            System.out.print(arr1[i][2]);
+            System.out.print(arr1[i][0]);
             System.out.println();
-
-//            System.out.print(arr1[i][1]);
-//            System.out.print(arr1[i][2]);
-//            System.out.print(arr1[i][0]);
-//            System.out.println();
-//            }
-//            System.out.print(arr1[0][1]);
-//            System.out.print(arr1[0][2]);
-//            System.out.print(arr1[0][0]);
-//            System.out.println();
         }
+        System.out.print(arr1[0][1]);
+        System.out.print(arr1[0][2]);
+        System.out.print(arr1[0][0]);
+        System.out.println();
         System.out.println("---------------");
+//        for (int i = 0; i < arr1.length; i++) {
+//            for (int j = 0; j < arr1.length; j++) {
+//                System.out.print(arr1[((i + 1) % 3)][(j + 1) % 3]);
+//            }
+//            System.out.println();
+//        }
+
 
 //      4
         for (int i = 1; i <= 2; i++) {
@@ -56,8 +52,14 @@ public class Main4 {
             System.out.print(arr1[0][1]);
             System.out.println();
 
-        }
+//        for (int i = 0; i < arr1.length; i++) {
+//            for (int j = 0; j < arr1.length; j++) {
+//                System.out.print(arr1[(i + 1) % 3][(j + 2) % 3]);
+//            }
+//            System.out.println();
 
         }
+
+}
 
 
